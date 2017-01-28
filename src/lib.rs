b9d6c6786a111036f764ddc6665f547fc7d8d1cb
Kai Rosswag <kairosswag@gmail.com>
2017-01-28 22:56:12 +0100

    Set up first steps for password estimation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,42 @@
+
+
+#[derive(Debug, PartialEq)]
+pub enum Weakness {
+    TooShort,
+    NoNumbers,
+    None
+}
+
+pub fn estimate_strenght(password: &str) -> (i32, Weakness) {
+    simple::estimate_by_lenght(password)
+}
+
+mod simple {
+    use super::Weakness;
+
+    /// estimates password strenght according to its lenght
+    pub fn estimate_by_lenght(password: &str) -> (i32, Weakness) {
+        let len = password.len();
+        match len {
+            1 ... 5 => (1, Weakness::TooShort),
+            _ => (10, Weakness::None),
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+
+    use super::Weakness;
+
     #[test]
     fn it_works() {
+    }
+
+    #[test]
+    fn test_too_short() {
+        let (_, weak) = super::estimate_strenght("abc");
+        assert_eq!(Weakness::TooShort, weak);
     }
 }
